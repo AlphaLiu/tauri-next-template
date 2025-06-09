56de354b058eb4e1bafefcbc5b7ad8065001a68c
Alpha Liu <alpha.liu@me.com>
2025-06-09 11:43:52 +0800

    feat: add tauri

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+
+const isProd = process.env.NODE_ENV === 'production';
+const internalHost = process.env.TAURI_DEV_HOST || 'localhost';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  // 配置 assetPrefix，否则服务器无法正确解析您的资产。
+  assetPrefix: isProd ? undefined : `http://${internalHost}:3000`,
 };
 
 export default nextConfig;
